e4dba6a92a7ba9488ab88d30de4263e360066958
Bailie Whetten <bailiewhetten@gmail.com>
2026-01-26 14:51:32 -0700

    commit CheckWinner()

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -22,7 +22,23 @@ namespace Mission4Assignment
         // contain a method that receives the game board array as input and returns if there is a winner and who it was
         public int CheckWinner(string board)
         {
-            return 0;
+            int winner = 0;
+            
+            // Check Rows
+            if (board[0] == board[1] && board[1] == board[2] && board[0] != ' ') winner = board[0] == 'X' ? 1 : 2;
+            if (board[3] == board[4] && board[4] == board[5] && board[3] != ' ') winner = board[3] == 'X' ? 1 : 2;
+            if (board[6] == board[7] && board[7] == board[8] && board[6] != ' ') winner = board[6] == 'X' ? 1 : 2;
+    
+            // Check Columns
+            if (board[0] == board[3] && board[3] == board[6] && board[0] != ' ') winner = board[0] == 'X' ? 1 : 2;
+            if (board[1] == board[4] && board[4] == board[7] && board[1] != ' ') winner = board[1] == 'X' ? 1 : 2;
+            if (board[2] == board[5] && board[5] == board[8] && board[2] != ' ') winner = board[2] == 'X' ? 1 : 2;
+    
+            // Check Diagonals
+            if (board[0] == board[4] && board[4] == board[8] && board[0] != ' ') winner = board[0] == 'X' ? 1 : 2;
+            if (board[2] == board[4] && board[4] == board[6] && board[2] != ' ') winner = board[2] == 'X' ? 1 : 2;
+    
+            return winner;
         }
     }
 }
