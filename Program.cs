092fdc2c9cc35e05f20bb2ec0dcd3cb73612deed
caydnbaldwin <caydnbaldwin@gmail.com>
2026-01-26 13:30:24 -0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿// welcome the user
+// create game board array to store players' choices
+// ask each player in turn for their choice
+// update the game board array
+// print the board by CALLING the method in the supporting class
+// check for a winner by CALLING the method in the supporting class
+// notify the players when a win has occurred and which player won the game
---
+++ TicTacToeTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission4Assignment
+{
+    internal class TicTacToeTools
+    {
+        // receive the board array from the driver class
+        // contain a method that prints the board based on the information passed to it
+        // contain a method that receives the game board array as in put and returns if there is a winner and who it was
+    }
+}
