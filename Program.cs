702b2dd3d82df8dd73949bd358707f770c15d26e
caydnbaldwin <caydnbaldwin@gmail.com>
2026-01-26 14:24:10 -0700

    Added the Print Board method inside the TicTacToeTools class.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿// welcome the user
+﻿using Mission4Assignment;
+TicTacToeTools t = new TicTacToeTools();
+
+// welcome the user
 // create game board array to store players' choices
 // ask each player in turn for their choice
 // update the game board array
+
 // print the board by CALLING the method in the supporting class
+// METHOD SIGNATURE -> public void PrintBoard(string board)
+t.PrintBoard("012345678");
+[
+    'xxx345678',
+    'ooo345678',
+]
+
 // check for a winner by CALLING the method in the supporting class
+// METHOD SIGNATURE -> public int CheckWinner(string board)
 // notify the players when a win has occurred and which player won the game
